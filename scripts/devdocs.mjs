af380f58e5b2b2799e315d492d6495317113f926
S222em <sem.roeten@gmail.com>
2021-10-30 21:05:01 +0200

    Fixes

--- scripts/devdocs.mjs
+++ scripts/devdocs.mjs
@@ -1,6 +1,5 @@
 import { runGenerator } from '@discordjs/ts-docgen';
 
 runGenerator({
-	existingOutput: 'docs/dev.json',
-	output: 'docs/docs.json'
+	output: 'docs/dev.json'
 });
--- scripts/docs.mjs
+++ scripts/docs.mjs
@@ -1,6 +1,5 @@
 import { runGenerator } from '@discordjs/ts-docgen';
 
 runGenerator({
-	existingOutput: 'docs/main.json',
-	output: 'docs/docs.json'
+	output: 'docs/main.json'
 });
