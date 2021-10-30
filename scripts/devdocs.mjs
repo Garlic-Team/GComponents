62863f45e5fb1c6086e735f670f22c52a247fd48
S222em <sem.roeten@gmail.com>
2021-10-30 21:01:44 +0200

    Fixes

--- scripts/devdocs.mjs
+++ scripts/devdocs.mjs
@@ -3,6 +3,5 @@ import { runGenerator } from '@discordjs/ts-docgen';
 runGenerator({
 	existingOutput: 'docs/typedoc-out.json',
 	custom: 'docs/index.yml',
-	output: 'docs/docs.json',
-	config: 'docs/index.yml'
+	output: 'docs/docs.json'
 });
