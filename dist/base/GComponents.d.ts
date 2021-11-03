f27e4128101e4384deb854b1bc8f56568d9c0e25
S222em <sem.roeten@gmail.com>
2021-11-03 16:44:31 +0100

    Remove dist

--- dist/base/GComponents.d.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Collection } from 'discord.js';
-import { Component } from '../structures/Component';
-import { GComponentsOptions } from '../util/Constants';
-export declare class GComponents {
-    client: Client;
-    dir: string;
-    components: Collection<string, Component>;
-    constructor(client: Client, options: GComponentsOptions);
-}
--- dist/managers/GComponentsLoader.d.ts
+++
@@ -1,9 +0,0 @@
-import { Client, Collection } from 'discord.js';
-import { Component } from '../structures/Component';
-export declare class GComponentsLoader {
-    private client;
-    private dir;
-    private components;
-    constructor(client: Client, components: Collection<string, Component>, dir: string);
-    __loadFiles(dir: string): Promise<void>;
-}
