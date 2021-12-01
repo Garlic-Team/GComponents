546b32da324c951d6ff2ea1d896a014a2a2f3853
S222em <sem.roeten@gmail.com>
2021-12-01 15:59:43 +0100

    Fix issue with arguments (numbers)

--- src/structures/CustomId.ts
+++ src/structures/CustomId.ts
@@ -2,21 +2,25 @@ export class CustomId {
     public name: string;
     public ids: Array<string>;
 
-    constructor(options: { name: string, ids: Array<string>}) {
+    constructor(options: { name: string, ids: string[] }) {
         this.name = options.name;
         this.ids = options.ids !== undefined ? options.ids : [];
     }
+
     get() {
         return `${this.name}${this.ids[0] ? `-${this.ids.join('-')}` : ''}`;
     }
+
     setName(name: string) {
         this.name = name;
         return this;
     }
+
     addId(id: string) {
         this.ids.push(id);
         return this;
     }
+
     addIds(ids: Array<string>) {
         for (const id in ids) {
             this.addId(id);
