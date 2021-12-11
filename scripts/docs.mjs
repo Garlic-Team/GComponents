529f59c02b0da19168ce42dc82985de137465471
Hyro <generalkubo@gmail.com>
2021-12-11 18:45:18 +0100

    Update docs.mjs

--- scripts/docs.mjs
+++ scripts/docs.mjs
@@ -1,7 +1,7 @@
 import { runGenerator } from '@discordjs/ts-docgen';
 
 runGenerator({
-    existingOutput: 'docs/out-main.json',
-	output: 'docs/main.json',
+    existingOutput: 'docs/out-master.json',
+	output: 'docs/master.json',
     custom: 'docs.yml'
 });
